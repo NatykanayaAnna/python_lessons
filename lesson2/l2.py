b24867ec2cf69d83675e1c1cd7686270d4f6e20e
NatykanayaAnna <annanatykanaya@gmail.com>
2022-04-01 16:47:38 +0300

    add homework

--- lesson2/l2.py
+++ lesson2/l2.py
@@ -3,6 +3,7 @@
 # result = pizza/people
 # print(result, "столько кусочков на человека.")
 # password = 1234
+#
 # while True:
 #     password_user = int(input("Введите пароль\n"))
 #     if password == password_user:
@@ -10,12 +11,21 @@
 #         break
 #     else:
 #         print("Пароль не правильный!")
-print("Птица, которая носит костюм - это...")
-right_word = "пингвин"
+#
+# print("Птица, которая носит костюм - это...")
+# right_word = "пингвин"
+# while True:
+#     user_word = input()
+#     if user_word == right_word:
+#         print("Да! Все верно!")
+#         break
+#     else:
+#         print("НЕ верно!")
 while True:
-    user_word = input()
-    if user_word == right_word:
-        print("Да! Все верно!")
+    user_number = int(input("guess a number from 1 to 10\n"))
+    right_number = 7
+    if user_number == right_number:
+        print("That's right!")
         break
     else:
-        print("НЕ верно!")
+        print("That's wrong. Try again!")
