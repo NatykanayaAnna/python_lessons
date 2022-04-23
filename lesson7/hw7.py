9bbd43988d272c13f38d073bffdb6ccc41073812
NatykanayaAnna <annanatykanaya@gmail.com>
2022-04-24 02:33:22 +0300

    finished homework

--- lesson7/hw7.py
+++ lesson7/hw7.py
@@ -4,31 +4,40 @@ import random
 def random_word():
     words_list = ["apple", "dog", "flower", "pineapple", "wolf"]
     rand_word = random.choice(words_list)
-    print(rand_word)
     return rand_word
 
 
 def lenght_word(rand_word):
-    print(rand_word)
     lenght = len(rand_word)
     count_letters = lenght * ' _ '
     print(count_letters)
+    return lenght
 
-
-lenght_word(random_word())
 
 guessed_letters = []
 word = random_word()
-while True:
+total_attemps = lenght_word(word) + 1
+used_attems = 0
+user_word = "0"
+while used_attems != total_attemps:
     guess = input("Enter a letter:\n")
-    if guess in word:
-        guessed_letters.append(guess)
-        print("You guess! The word have this letter\nYou guessed that letters:", guessed_letters)
-        word_or_letter = int(input("Enter '1' if you want to guess the word, or '0' for continue guessing the letters:\n"))
-        if word_or_letter == 1:
+    used_attems += 1
+    if guess.lower() in word:
+        if guess in guessed_letters:
+            print("Ты уже угадал эту букву", guessed_letters)
+        else:
+            guessed_letters.append(guess.lower())
+            print("You guess! The word have this letter\nYou guessed that letters:", guessed_letters)
+        while True:
+            input_word_or_letter = input(
+                "Enter '1' if you want to guess the word, or '0' for continue guessing the letters:\n")
+            if input_word_or_letter == "1" or input_word_or_letter == "0":
+                break
+            else:
+                print("I don't understand")
+        if input_word_or_letter == "1":
             user_word = input("Enter the word:\n")
-            if user_word == word:
-                print("This is right! You win!")
+            if user_word.lower() == word:
                 break
             else:
                 print("This wrong")
@@ -36,3 +45,7 @@ while True:
             continue
     else:
         print("This letter is not in the word")
+if user_word.lower() == word:
+    print("You win!")
+else:
+    print("You used all attems. The game is over")
