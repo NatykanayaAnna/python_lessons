1e777e325045fe55dc9c6f3d2f7023f251664444
Veliade Saliieva <vsaliieva@MacBook-Air-Veliade.local>
2022-04-28 18:56:10 +0300

    Add line width

--- game/tic-tac-toe.py
+++ game/tic-tac-toe.py
@@ -1,17 +1,21 @@
 from graphics import *
 
 win = GraphWin('lesson_1', 600, 600)
-
+win.setBackground('white')
 
 def field():
     # vertical
     line_1 = Line(Point(200, 100), Point(200, 400))
+    line_1.setWidth(3)
     line_1.draw(win)
     line_2 = Line(Point(300, 100), Point(300, 400))
+    line_2.setWidth(3)
     line_2.draw(win)
     line_3 = Line(Point(100, 200), Point(400, 200))
+    line_3.setWidth(3)
     line_3.draw(win)
     line_4 = Line(Point(100, 300), Point(400, 300))
+    line_4.setWidth(3)
     line_4.draw(win)
 
 
